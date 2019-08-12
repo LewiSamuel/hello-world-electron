203acfea428738aa0ad057c5a16da3cf1bea1144
Lewi Samuel <arroba.lewi@gmail.com>
2019-08-12 18:54:25 -0300

    create directory dinamically

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, } = require('electron');
+var fs = require('fs');
+const fse = require('fs-extra');
+// Main Process
+
 
 function createWindow () {
+
+
+
+  // var dir = './teste';
+
+  // if (!fs.existsSync(dir)){
+  //     fs.mkdirSync(dir);
+  // }
+
+  // fs.appendFile("./teste/mynewfile1.txt", 'Hello content!', function (err) {
+  //   if (err) throw err;
+  //   console.log('Saved!');
+  // });
+
+
+  // fse.move('./teste', '/teste', err => {
+  //   if(err) return console.error(err);
+  //   console.log('success!');
+  // });
+
   // Cria uma janela de navegação.
   let win = new BrowserWindow({
     width: 800,
@@ -11,7 +35,9 @@ function createWindow () {
   })
 
   // e carregar o index.html do aplicativo.
-  win.loadFile('index.html')
+  win.loadFile('index.html');
+  console.log(win.webContents.getURL());
+
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
